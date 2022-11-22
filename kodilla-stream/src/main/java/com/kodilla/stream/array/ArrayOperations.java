f164c005e74d44e5d26a3309ed577ffa7e2153de
Jakub Hryckiewicz <jak-hry@wp.pl>
2022-11-22 15:38:10 +0100

    7.6 Added

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -4,7 +4,7 @@ import java.util.stream.IntStream;
 
 public interface ArrayOperations {
 
-    static double getAverage(int[] numbers){
+    static double getAverage(int[] numbers) {
 
         IntStream.range(0, numbers.length)
                 .map(n -> numbers[n])
