151d23e4f2cf06ad33c1f5c27b66c75905eae0a2
Jakub Hryckiewicz <jak-hry@wp.pl>
2022-11-22 22:48:37 +0100

    8.3 Added

--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -1,12 +1,19 @@
 package com.kodilla.exception.main;
 
 import com.kodilla.exception.io.FileReader;
+import com.kodilla.exception.io.FileReaderException;
 
 public class ExceptionModuleRunner {
 
     public static void main(String[] args) {
 
         FileReader fileReader = new FileReader();
-        fileReader.readFile();
+        final String fileName = null;
+
+        try {
+            fileReader.readFile(fileName);
+        } catch (FileReaderException e) {
+            System.out.println("Problem while reading a file!");
+        }
     }
 }
---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,19 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling extends Exception {
+
+    public static void main(String[] args) {
+
+        SecondChallenge secondChallenge = new SecondChallenge();
+        String result = "no result";
+
+        try {
+            result = secondChallenge.probablyIWillThrowException(2, 1.5);
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        } finally {
+            System.out.println(result);
+        }
+    }
+
+}
