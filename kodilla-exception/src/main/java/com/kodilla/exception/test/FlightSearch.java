171377fa4e4e8a14a224bc8bc9cd8d5aad211d1b
Jakub Hryckiewicz <jak-hry@wp.pl>
2022-12-20 23:10:14 +0100

    8.4 poprawki

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearch.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearch.java
@@ -9,14 +9,11 @@ public class FlightSearch {
         HashMap<String, Boolean> flightsMap = new HashMap<>();
 
         flightsMap.put("Barcelona", true);
-        flightsMap.put("Warsaw", true);
+        flightsMap.put("Warsaw", false);
         flightsMap.put("London", true);
-        flightsMap.put("Paris", true);
+        flightsMap.put("Paris", false);
         flightsMap.put("Madrid", true);
         flightsMap.put("Roma", true);
-        flightsMap.put("Berlin", false);
-        flightsMap.put("Dublin", false);
-        flightsMap.put("Amsterdam", false);
 
         if (flightsMap.containsKey(flight.getArrivalAirport()) && flightsMap.containsKey(flight.getDepartureAirport())) {
             return true;
