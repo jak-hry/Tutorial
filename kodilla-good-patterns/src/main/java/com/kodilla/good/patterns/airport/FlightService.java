5828c3e38cbc295502df1551b1c028b8ad90eaf6
Jakub Hryckiewicz <jak-hry@wp.pl>
2023-01-07 23:32:39 +0100

    13.5 corrections

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/airport/FlightService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/airport/FlightService.java
@@ -1,8 +1,8 @@
 package com.kodilla.good.patterns.airport;
 
 import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class FlightService {
 
@@ -24,10 +24,16 @@ public class FlightService {
 
     public void searchThroughAnotherCity(HashSet<Airport> list, String airportNameFrom, String airportNameTo, String anotherCity) {
 
-        list.stream()
+        Set<Airport> result = list.stream()
                 .filter(route -> route.getAirportNameFrom().equals(airportNameFrom))
                 .filter(route -> route.getAirportNameTo().equals(airportNameTo))
                 .collect(Collectors.toSet());
 
+        if (result.size() > 0) {
+            Airport airport = new Airport(airportNameTo, anotherCity);
+            list.add(airport);
+            System.out.println("flight from \"" + airportNameFrom +
+                    "\" through \"" + anotherCity + "\" to \"" + airportNameTo + "\"");
+        }
     }
 }
