992660e66a66086e45baabd6fd6b3ea29600a81a
Jakub Hryckiewicz <jak-hry@wp.pl>
2023-01-11 16:50:32 +0100

    14.2 corrections

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -11,19 +11,23 @@ public class Calculator {
 
 
     public double add(double a, double b) {
-        return display.displayValue(a + b);
+        display.displayValue(a + b);
+        return a + b;
     }
 
     public double sub(double a, double b) {
-        return display.displayValue(a - b);
+        display.displayValue(a - b);
+        return a - b;
     }
 
     public double mul(double a, double b) {
-        return display.displayValue(a * b);
+        display.displayValue(a * b);
+        return a * b;
     }
 
     public double div(double a, double b) {
-        return display.displayValue(a / b);
+        display.displayValue(a / b);
+        return a / b;
     }
 
 }
