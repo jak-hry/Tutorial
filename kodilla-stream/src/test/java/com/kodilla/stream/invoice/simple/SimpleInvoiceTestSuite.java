e5cb25d6abbf0552457d671b55b217c22f18de7c
Jakub Hryckiewicz <jak-hry@wp.pl>
2023-01-24 19:07:31 +0100

    16.3 Added

--- kodilla-stream/src/test/java/com/kodilla/stream/invoice/simple/SimpleInvoiceTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/invoice/simple/SimpleInvoiceTestSuite.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class SimpleInvoiceTestSuite {
 
     @Test
-    void testGetValueToPay(){
+    public void testGetValueToPay(){
 
         //Given
         SimpleInvoice invoice = new SimpleInvoice();
