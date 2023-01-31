533539f440d160ffcbf40f693a9caa8ba4be334a
Jakub Hryckiewicz <jak-hry@wp.pl>
2023-01-31 19:38:00 +0100

    17.2 Added

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
@@ -0,0 +1,58 @@
+package com.kodilla.hibernate.tasklist;
+
+import com.kodilla.hibernate.task.Task;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "TASKSLISTS")
+public final class TaskList {
+
+
+    private int id;
+    private String listName;
+    private String description;
+
+    public TaskList() {
+    }
+
+    public TaskList(String listName, String description) {
+        this.id = id;
+        this.listName = listName;
+        this.description = description;
+    }
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ID", unique = true)
+    public int getId() {
+        return id;
+    }
+
+    @NotNull
+    @Column(name = "LISTNAME")
+    public String getListName() {
+        return listName;
+    }
+
+    @Column(name = "DESCRIPTION")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setListName(String listName) {
+        this.listName = listName;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -0,0 +1,43 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class TaskListDaoTestSuite {
+
+    @Autowired
+    TaskListDao taskListDao;
+
+    @Test
+    void testFindByListName() {
+
+        //Given
+        String listName = "taskInProgress";
+        TaskList taskList = new TaskList(listName, "17.2_Task");
+        TaskList taskList2 = new TaskList(listName, "17.2_Tests");
+
+        //When
+        taskListDao.save(taskList);
+        taskListDao.save(taskList2);
+
+        //Then
+        List<TaskList> readTasks = taskListDao.findByListName(listName);
+        assertEquals(2, readTasks.size());
+        assertEquals(listName, readTasks.get(0).getListName());
+        assertEquals(listName, readTasks.get(1).getListName());
+
+        //CleanUp
+        int id1 = readTasks.get(0).getId();
+        int id2 = readTasks.get(1).getId();
+        taskListDao.deleteById(id1);
+        taskListDao.deleteById(id2);
+    }
+}
