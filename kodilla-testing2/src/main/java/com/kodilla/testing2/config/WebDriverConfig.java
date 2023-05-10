1041e148da04248c567a064a736e9eb33613d465
Jakub Hryckiewicz <jak-hry@wp.pl>
2023-05-10 18:35:46 +0200

    31.1 Added

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/config/WebDriverConfig.java
@@ -0,0 +1,21 @@
+package com.kodilla.testing2.config;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+public class WebDriverConfig {
+    public final static String FIREFOX = "FIREFOX_DRIVER";
+    public final static String CHROME = "CHROME_DRIVER";
+    public static WebDriver getDriver(final String driver) {
+        System.setProperty("webdriver.gecko.driver", "C:\\Selenium-drivers\\Firefox\\geckodriver-v0.33.0-win32\\geckodriver.exe");
+        System.setProperty("webdriver.chrome.driver", "c:\\Selenium-drivers\\Chrome\\chromedriver_win32\\chromedriver.exe");
+
+        if (driver.equals(FIREFOX)) {
+            return new FirefoxDriver();
+        } else if (driver.equals(CHROME)) {
+            return new ChromeDriver();
+        } else {
+            return null;
+        }
+    }
+}
